5c7e5d46b2ed520acc4d343867fff166231a7e41
Sven Reymann <reymann78@freenet.de>
2020-11-16 12:18:51 +0100

    rename application.properties to application.yml delete react README.md, logo.svg, index.css, App.css clean up App.js and change title in index.html, add proxy localhost 8080 to package.json, adapted App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders All Your Favorites', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/All Your Favorites/i);
   expect(linkElement).toBeInTheDocument();
 });
